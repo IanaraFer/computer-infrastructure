5e9e420c7ee7218ae681651aedb06a7d1b27e0c6
Ianara Fernandes <158167439+IanaraFer@users.noreply.github.com>
2024-12-12 10:21:29 +0000

    py

--- weather-data.py
+++ weather-data.py
@@ -5,13 +5,15 @@ import schedule
 import time
 from datetime import datetime
 # Downlow weather web pag.
-
-
+    
+    
 def fetch_weather_data():
     url = 'https://prodapi.metweb.ie/observations/athenry/today'
     response = requests.get(url)
     if response.ok:
         data = response.json()  # Assuming the response is in JSON format
+        if isinstance(data, list):
+            data = data[0]  # Extract the first item if it's a list
         weather_info = {
             "datetime": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
             "temperature": data.get('temperature'),
@@ -22,11 +24,3 @@ def fetch_weather_data():
         print(weather_info)
     else:
         print("Failed to retrieve the weather data")
-
-# Schedule the task at 10 AM everyday
-schedule.every().day.at("10:00").do(fetch_weather_data)
-
-print("Scheduler started. Waiting for the job to run at 10:00 AM every day.")
-while True:
-    schedule.run_pending()
-    time.sleep(60)
